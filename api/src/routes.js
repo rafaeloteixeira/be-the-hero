f58196bd77d156701b17c1c8382111261b3625d2
Rafael Teixeira <rafael.teixeira@ambar.tech>
2020-06-22 17:18:24 -0300

    Add backend

---
+++ api/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = express.Router();
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index );
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profiles', ProfileController.index);
+
+routes.post('/sessions', SessionController.create);
+
+module.exports = routes
